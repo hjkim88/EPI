d0995f6fc42c3a83df6c5efa9296a5191057aec6
Hyunjin Kim <firadazer@gmail.com>
2018-11-29 00:35:27 -0500

    Initial commit

---
+++ codes/p24.py
@@ -0,0 +1,24 @@
+###
+#   File name  : p24.py
+#   Author     : Hyunjin Kim
+#   Date       : Nov 29, 2018
+#   Email      : firadazer@gmail.com
+#   Purpose    : How would you compute the parity of a very large number of 64-bit words?
+#   Background : The parity of a binary word is 1 if the number of 1s in the word is odd;
+#                otherwise, it is 0. For example, the parity of 1011 is 1, and the parity of 10001000 is 0.
+#                Parity checks are used to detect single bit errors in data storage and communication.
+#                It is fairly straightforward to write code that computes the parity of a single 64-bit word.
+#
+#   Instruction
+#               1. import p24
+#               2. Run the function p24.start()
+#               3. The results will be generated in the console
+###
+
+def start():
+    print("p24.py")
+
+
+start()
+
+
