af3b9d1f8d52b09f8d456059dd1e2f5fe42b2e29
hjkim88 <firadazer@gmail.com>
2019-01-18 17:50:14 -0500

    4.3 reverse bits Implemented my own codes. A hint has been given and I should think more.

---
+++ codes/4_3_p28.py
@@ -0,0 +1,68 @@
+###
+#   File name  : 4_3_p28.py
+#   Author     : Hyunjin Kim
+#   Date       : Jan 18, 2018
+#   Email      : firadazer@gmail.com
+#   Purpose    : Write a program that takes a 64-bit word and returns the 64-bit word consisting of
+#                the bits of the input word in reverse order.
+#   Example    : If the input is (1110000000000001), the output should be (1000000000000111).
+#
+#   Instruction
+#               1. import 4_3_p28
+#               2. Run the function 4_3_p28.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+
+### a function starting this script
+def start():
+    print("4_3_p28.py")
+
+    start_time = timeit.default_timer()
+    print("reverse_bit(01011010, 8) = ", bin(reverse_bit(int('01011010', 2), 8)))
+    print("reverse_bit(1110000000000001, 16) = ", bin(reverse_bit(int('1110000000000001', 2), 16)))
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+    start_time = timeit.default_timer()
+    print("reverse_bit2(01011010, 8) = ", bin(reverse_bit2(int('01011010', 2), 8)))
+    print("reverse_bit2(1110000000000001, 16) = ", bin(reverse_bit2(int('1110000000000001', 2), 16)))
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+### a brute-force function to reverse the bits of the input (n-bit word)
+### save n bits and remake one with the reversed order
+def reverse_bit(x, n):
+    y = 0
+
+    for i in range(n):
+        if((1 & (x >> i)) == 1):
+            y |= 1 << (n-i-1)
+
+    return y
+
+
+### a function to reverse the bits of the input with bit swapping
+### if the rightest and the leftest bits are the same, no need to change them
+def reverse_bit2(x, n):
+    for i in range(n//2):
+        if(((x >> (n-i-1)) & 1) != ((x >> i) & 1)):
+            bit_mask = (1 << (n-i-1)) | (1 << i)
+            x ^= bit_mask
+
+    return x
+
+
+### hint: use a lookup table
+
+
+### a more efficient function to reverse the bits of the input
+### using a lookup table
+def reverse_bit3(x,n):
+
+    return x
+
+
+start()
