511bf33e2a2d8db056161f5b41945dc84522b669
hjkim88 <firadazer@gmail.com>
2019-01-18 13:35:21 -0500

    4_2_p27.py newly added. - initial commit.

---
+++ codes/4_2_p27.py
@@ -0,0 +1,30 @@
+###
+#   File name  : 4_2_p27.py
+#   Author     : Hyunjin Kim
+#   Date       : Jan 18, 2018
+#   Email      : firadazer@gmail.com
+#   Purpose    : Implement code that takes an input as 64-bit integer and swaps the bits at indices i and j.
+#   Background : A 64-bit integer can be viewed as an array of 64 bits, with the bit at index 0
+#                corresponding to the least significant bit (LSB), and the bit at index 63
+#                corresponding to the most significant bit (MSB).
+#                e.g., the 8-bit integer 73 can be viewed as array of bits, with the LSB being at index 0.
+#                 0  1   0   0   1   0   0   1
+#                MSB                        LSB
+#
+#   Instruction
+#               1. import 4_2_p27
+#               2. Run the function 4_2_p27.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+
+### a function starting this script
+def start():
+    print("4_2_p27.py")
+
+
+
+
