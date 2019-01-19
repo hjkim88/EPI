8b6f4eccde7019a05cf39ecb4a62fc46373ae183
Hyunjin Kim <firadazer@gmail.com>
2019-01-19 15:47:52 -0500

    A new approach using a lookup table has been added. Hooray!

--- codes/4_3_p28.py
+++ codes/4_3_p28.py
@@ -59,10 +59,14 @@ def reverse_bit2(x, n):
 
 
 ### a more efficient function to reverse the bits of the input
-### using a lookup table
-def reverse_bit3(x,n):
-
-    return x
+### using a lookup table - 64bit-input only
+# def reverse_bit3(x):
+#     MASK_SIZE = 16
+#     BIT_MASK = 0xFFFF
+#     return (PRECOMPUTED_REVERSE[x >> (3 * MASK_SIZE)]   |
+#              (PRECOMPUTED_REVERSE[(x >> (2 * MASK_SIZE)) & BIT_MASK] << MASK_SIZE) |
+#              (PRECOMPUTED_REVERSE[(x >> MASK_SIZE) & BIT_MASK] << (2 * MASK_SIZE)) |
+#              (PRECOMPUTED_REVERSE[x & BIT_MASK] << (3 * MASK_SIZE)))
 
 
 start()
