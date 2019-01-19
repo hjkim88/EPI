c4b97fbf596ccd88a5c576264614ef7595d62540
Hyunjin Kim <firadazer@gmail.com>
2019-01-19 15:47:14 -0500

    Some minor changes.

--- codes/4_1_p24.py
+++ codes/4_1_p24.py
@@ -128,8 +128,8 @@ def parity2(x):
 #     MASK_SIZE = 16
 #     BIT_MASK = 0xFFFF
 #     return(PRECOMPUTED_PARITY[x >> (3 * MASK_SIZE)] ^
-#            PRECOMPUTED_PARITY[(x >> (2 * MASK_SIZE)) & BIT_MASK] ^
-#            PRECOMPUTED_PARITY[(x >> MASK_SIZE) & BIT_MASK] ^
+#            PRECOMPUTED_PARITY[((x >> (2 * MASK_SIZE))) & BIT_MASK] ^
+#            PRECOMPUTED_PARITY[((x >> MASK_SIZE)) & BIT_MASK] ^
 #            PRECOMPUTED_PARITY[x & BIT_MASK])
 
 
