a6466125d2e2d9f3779df96bf02eaccc52003c71
hjkim88 <firadazer@gmail.com>
2019-01-23 17:49:27 -0500

    section 4.7 in p32. complete.

--- codes/Primitive_Types/4_7_p32.py
+++ codes/Primitive_Types/4_7_p32.py
@@ -22,11 +22,53 @@ def start():
     print("4_7_p32.py")
 
     start_time = timeit.default_timer()
-    print()
+    print("exponentiation(2, 3) = ", exponentiation(2, 3))
+    print("exponentiation(5, 0) = ", exponentiation(5, 0))
+    print("exponentiation(23, 2) = ", exponentiation(23, 2))
+    print("exponentiation(2.7, 4) = ", exponentiation(2.7, 4))
+    print("exponentiation(10.1, 5) = ", exponentiation(10.1, 5))
+    for i in range(10000):
+        exponentiation(1.5, 30)
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+    start_time = timeit.default_timer()
+    print("exponentiation2(2, 3) = ", exponentiation2(2, 3))
+    print("exponentiation2(5, 0) = ", exponentiation2(5, 0))
+    print("exponentiation2(23, 2) = ", exponentiation2(23, 2))
+    print("exponentiation2(2.7, 4) = ", exponentiation2(2.7, 4))
+    print("exponentiation2(10.1, 5) = ", exponentiation2(10.1, 5))
+    for i in range(10000):
+        exponentiation2(1.5, 30)
     print("Execution Time: ", timeit.default_timer() - start_time)
 
 
 ### a brute-force function to calculate x**y without using **
-def getExponent(x, y):
-    return(x**y)
+def exponentiation(x, y):
+    result = x
+    while(y > 1):
+        result *= x
+        y -= 1
 
+    if(y == 0):
+        result = 1
+
+    return result
+
+
+### a more efficient function than the brute-force one
+### the exponent can be transfomed as binary number
+### it is like binary search
+### x**27 = x**16 + x**8 + x**2 + x**1
+### here, x**16 = x**8 x x**8 = x**4 x x**4 x x**4 x x**4
+def exponentiation2(x, y):
+    result = 1
+    while(y):
+        if(y & 1 == 1):
+            result *= x
+        x *= x
+        y >>= 1
+
+    return result
+
+
+start()
