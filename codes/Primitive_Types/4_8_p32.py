417425c30da609353e3ef33d0c384c5f58946305
hjkim88 <firadazer@gmail.com>
2019-01-25 17:34:19 -0500

    reverse_digit() and reverse_digit2() have been added.

--- codes/Primitive_Types/4_8_p32.py
+++ codes/Primitive_Types/4_8_p32.py
@@ -22,13 +22,60 @@ def start():
     print("4_8_p32.py")
 
     start_time = timeit.default_timer()
-    print()
+    print("reverse_digit(42) = ", reverse_digit(42))
+    print("reverse_digit(-314) = ", reverse_digit(-314))
+    print("reverse_digit(5) = ", reverse_digit(5))
+    print("reverse_digit(329573) = ", reverse_digit(329573))
+    print("reverse_digit(-9837434) = ", reverse_digit(-9837434))
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+    start_time = timeit.default_timer()
+    print("reverse_digit2(42) = ", reverse_digit(42))
+    print("reverse_digit2(-314) = ", reverse_digit(-314))
+    print("reverse_digit2(5) = ", reverse_digit(5))
+    print("reverse_digit2(329573) = ", reverse_digit(329573))
+    print("reverse_digit2(-9837434) = ", reverse_digit(-9837434))
     print("Execution Time: ", timeit.default_timer() - start_time)
 
 
-### a brute-force function to return reversed digit of a given integer
-def reverseDigit(x):
-    return(x)
+### a function to return reversed digit of a given integer
+def reverse_digit(x):
+    ispositive = True
+    if x < 0:
+        ispositive = False
+        x = abs(x)
+
+    n = 0
+    while x > 10**n:
+        n += 1
+
+    y = 0
+    for i in range((n-1), -1, -1):
+        y += (x // (10**i)) * 10**(n-i-1)
+        x = x % (10**i)
+
+    if ispositive:
+        return y
+    else:
+        return -y
+
+
+### hint: how would you solve the same problem if the input is presented as a string?
+
+
+### a function to change input integer to string and reverse it - based on the hint
+def reverse_digit2(x):
+    ispositive = True
+    if x < 0:
+        ispositive = False
+        x = abs(x)
+
+    x = str(x)
+
+    if ispositive:
+        return x[::-1]
+    else:
+        return "-" + x[::-1]
 
 
 start()
