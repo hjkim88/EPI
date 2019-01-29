3559c54cb9538100032b28fbbe1257f73bcdc976
Hyunjin Kim <firadazer@gmail.com>
2019-01-28 21:25:44 -0500

    section 4.11 in p35. - initial commit

---
+++ codes/Primitive_Types/4_11_p35.py
@@ -0,0 +1,27 @@
+###
+#   File name  : 4_11_p35.py
+#   Author     : Hyunjin Kim
+#   Date       : Jan 28, 2018
+#   Email      : firadazer@gmail.com
+#   Purpose    : Write a program which tests if two rectangles have a nonempty intersection.
+#                If the intersection is nonempty, return the rectangle formed by their intersection.
+#
+#   Background : This problem is concerned with rectangles whose sides are parallel to the X-axis and Y-axis.
+#
+#   Instruction
+#               1. import 4_11_p35
+#               2. Run the function 4_11_p35.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+
+### a function starting this script
+def start():
+    print("4_11_p35.py")
+
+
+
+
