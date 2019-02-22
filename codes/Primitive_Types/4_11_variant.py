7b4a9f6773c7da17756a368cba296dd36e8e0e3e
Hyunjin Kim <firadazer@gmail.com>
2019-02-22 00:46:57 -0500

    section 4.11 variants - initial commit.

---
+++ codes/Primitive_Types/4_11_variant.py
@@ -0,0 +1,35 @@
+###
+#   File name  : 4_11_variant.py
+#   Author     : Hyunjin Kim
+#   Date       : Feb 22, 2018
+#   Email      : firadazer@gmail.com
+#   Purpose    : 1. Given four points in the plane, how would you check if they are the vertices of a rectangle?
+#                2. How would you check if two rectangles, not necessarily aligned with the X and Y axes, intersect?
+#
+#   Background : This is related to the "4_11_p35.py".
+#
+#   Instruction
+#               1. import 4_11_variant
+#               2. Run the function 4_11_variant.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+
+### a function starting this script
+def start():
+    print("4_11_p35.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+### a function to check that the four given points represent a rectangle
+def fun_one():
+    return None
+
+
+start()
