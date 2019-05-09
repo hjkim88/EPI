979764fb55cd3ff7b9661a7a2d0b87f0bd23e6fe
Hyunjin Kim <firadazer@gmail.com>
2019-05-09 00:18:32 -0400

    5_1_p39 in implementation2 - the first function described in the book.

--- codes/Arrays/5_1_p39.py
+++ codes/Arrays/5_1_p39.py
@@ -59,5 +59,28 @@ def rearrange(A, i):
 
     return A
 
+### the function introduced in the book
+RED, WHITE, BLUE = range(3)
+def dutch_flag_partition(pivot_index, A):
+    pivot = A[pivot_index]
+    # First pass: group elements smaller than pivot
+    for i in range(len(A)):
+        # Look for a smaller element
+        for j in range(i+1, len(A)):
+            if A[j] < pivot:
+                A[i], A[j] = A[j], A[i]
+                break
+    # Scond pass: group elements larger than pivot
+    for i in reversed(range(len(A))):
+        if A[i] < pivot:
+            break
+        # Look for a larger element. Stop when we reach an element less than
+        # pivot, since first pass has moved them to the start of A
+        for j in reversed(range(i)):
+            if A[j] > pivot:
+                A[i], A[j] = A[j], A[i]
+                break
+
+
 
 start()
