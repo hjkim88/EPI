b43e50fcaa7600e0fb2754db58dab29c3faf5fd7
Hyunjin Kim <firadazer@gmail.com>
2019-05-13 11:20:55 -0400

    skeleton of 5_3_p43.py has been constructed.

---
+++ codes/Arrays/5_3_p43.py
@@ -0,0 +1,33 @@
+###
+#   File name  : 5_3_p43.py
+#   Author     : Hyunjin Kim
+#   Date       : May 13, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : Write a program that takes two arrays representing integers, and returns an integer
+#                representing their product.
+#   Example    : 193707721 x -761838257287 = -147573952589676412927. They should be all in arrays.
+#
+#   Instruction
+#               1. import 5_3_p43.py
+#               2. Run the function 5_3_p43.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+### a function starting this script
+def start():
+    print("5_3_p43.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+def array_product(x, y):
+
+    return 0
+
+
+start()
