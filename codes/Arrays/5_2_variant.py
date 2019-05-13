b17dfd9de5d4232ad854652c03a63a6fd8507eed
Hyunjin Kim <firadazer@gmail.com>
2019-05-13 11:15:25 -0400

    skeleton of 5_2_variant.py has been constructed.

---
+++ codes/Arrays/5_2_variant.py
@@ -0,0 +1,35 @@
+###
+#   File name  : 5_2_variant.py
+#   Author     : Hyunjin Kim
+#   Date       : May 13, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : 1. Write a program which takes as input two strings s and t of bits
+#                   encoding binary numbers Bs and Bt, respectively, and returns
+#                   a new string of bits representing the number Bs + Bt.
+#
+#   Background : This is related to the "5_2_p43.py".
+#
+#   Instruction
+#               1. import 5_2_variant
+#               2. Run the function 5_2_variant.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+### a function starting this script
+def start():
+    print("5_2_variant.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+def fun_one(Bs, Bt):
+
+    return 0
+
+
+start()
