82ad6e6562764de80109c8c9025231eb9d14cfcd
Hyunjin Kim <firadazer@gmail.com>
2019-05-26 15:35:26 -0400

    5_4_variant.py has been implemented.

--- codes/Arrays/5_4_variant.py
+++ codes/Arrays/5_4_variant.py
@@ -10,7 +10,7 @@
 #
 #   Example    : if A = [3, 3, 1, 0, 2, 0, 1], return 4
 #                if A = [3, 2, 0, 0, 2, 0, 1], return 0
-#                if A = [10, 3, 1, 0, 2, 0, 1], return 1
+#                if A = [10, 3, 1, 0, 2, 0, 1], return 2
 #                if A = [1, 1, 1, 1, 1, 1, 1], return 7
 #
 #
@@ -28,14 +28,31 @@ def start():
     print("5_4_variant.py")
 
     start_time = timeit.default_timer()
-    print("")
+    print("minimum_step([3, 3, 1, 0, 2, 0, 1]) = ", minimum_step([3, 3, 1, 0, 2, 0, 1]))
+    print("minimum_step([3, 2, 0, 0, 2, 0, 1]) = ", minimum_step([3, 2, 0, 0, 2, 0, 1]))
+    print("minimum_step([10, 3, 1, 0, 2, 0, 1]) = ", minimum_step([10, 3, 1, 0, 2, 0, 1]))
+    print("minimum_step([1, 1, 1, 1, 1, 1, 1]) = ", minimum_step([1, 1, 1, 1, 1, 1, 1]))
+    print("minimum_step([0, 3, 1, 0, 2, 0, 1]) = ", minimum_step([0, 3, 1, 0, 2, 0, 1]))
     print("Execution Time: ", timeit.default_timer() - start_time)
 
 
 ### a function to calculate minimum steps to advance to the last index
 def minimum_step(A):
+    old_max_idx = 0
+    max_idx = 0
+    B = [0] * len(A)
+    B[0] = 1
 
-    return 0
+    for i in range(len(A)):
+        if i > max_idx:
+            break
+        else:
+            max_idx = max(max_idx, (i + A[i]))
+            max_idx = min(max_idx, len(A)-1)
+            B[(old_max_idx+1):max_idx+1] = [B[i] + 1] * (max_idx - old_max_idx)
+            old_max_idx = max_idx
+
+    return B[len(B)-1]
 
 
 start()
