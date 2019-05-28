6a09f18513a5006ca7b953aef510a401348acfa6
Hyunjin Kim <firadazer@gmail.com>
2019-05-28 19:06:46 -0400

    Skeleton of 5_6_p46.py

---
+++ codes/Arrays/5_6_p46.py
@@ -0,0 +1,29 @@
+###
+#   File name  : 5_6_p46.py
+#   Author     : Hyunjin Kim
+#   Date       : May 28, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : Write a program that takes an array denoting the daily stock price, and returns the maximum
+#                profit that could be made by buying and then selling one share of that stock.
+#
+#   Example    : input: [310, 315, 275, 295, 260, 270, 290, 230, 255, 250] -> output: 30
+#
+#   Instruction
+#               1. import 5_6_p46.py
+#               2. Run the function 5_6_p46.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+### a function starting this script
+def start():
+    print("5_6_p46.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+start()
