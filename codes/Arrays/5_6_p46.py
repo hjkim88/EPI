1a52d0c4fa05614635c3da669f563e9dbf170f0b
Hyunjin Kim <firadazer@gmail.com>
2019-05-28 20:28:14 -0400

    5_6_p46.py completed.

--- codes/Arrays/5_6_p46.py
+++ codes/Arrays/5_6_p46.py
@@ -22,8 +22,25 @@ def start():
     print("5_6_p46.py")
 
     start_time = timeit.default_timer()
-    print("")
+    print("maximum_profit([310, 315, 275, 295, 260, 270, 290, 230, 255, 250]) = ", maximum_profit([310, 315, 275, 295, 260, 270, 290, 230, 255, 250]))
+    print("maximum_profit([3, 2, 5, 1, 2, 3, 1, 4, 5, 6, 2, 3]) = ", maximum_profit([3, 2, 5, 1, 2, 3, 1, 4, 5, 6, 2, 3]))
+    print("maximum_profit([100, 200, 300, 400, 500]) = ", maximum_profit([100, 200, 300, 400, 500]))
+    print("maximum_profit([500, 400, 300, 200, 100]) = ", maximum_profit([500, 400, 300, 200, 100]))
+    print("maximum_profit([100, 100, 100, 100, 100]) = ", maximum_profit([100, 100, 100, 100, 100]))
+    print("maximum_profit([450, 200, 480, 235, 200, 695, 390, 560, 240, 585]) = ", maximum_profit([450, 200, 480, 235, 200, 695, 390, 560, 240, 585]))
     print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+### a function to return the highest profit in the given stock array
+### one pass - get min values and take the highest current value - min
+def maximum_profit(A):
+    min_value = A[0]
+    profit = 0
+    for i in range(1, len(A)):
+        profit = max(profit, A[i]-min_value)
+        min_value = min(min_value, A[i])
+
+    return profit
 
 
 start()
