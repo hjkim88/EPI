31cd2209d64962f1b583a5bcd80c0b141797d7cd
Hyunjin Kim <firadazer@gmail.com>
2019-05-28 20:28:34 -0400

    Skeleton of 5_6_variant.py

---
+++ codes/Arrays/5_6_variant.py
@@ -0,0 +1,29 @@
+###
+#   File name  : 5_6_variant.py
+#   Author     : Hyunjin Kim
+#   Date       : May 28, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : 1. Write a program that takes an array of integers and finds the length of a longest subarray
+#                all of whose entries are equal.
+#
+#   Background : This is related to the "5_6_p46.py".
+#
+#   Instruction
+#               1. import 5_6_variant
+#               2. Run the function 5_6_variant.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+### a function starting this script
+def start():
+    print("5_6_variant.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+start()
