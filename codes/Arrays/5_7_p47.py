cc876e3e999a9a18b43d85df61b782fa32df2e5d
Hyunjin Kim <firadazer@gmail.com>
2019-05-29 21:10:10 -0400

    Skeleton of 5_7_p47.py

---
+++ codes/Arrays/5_7_p47.py
@@ -0,0 +1,29 @@
+###
+#   File name  : 5_7_p47.py
+#   Author     : Hyunjin Kim
+#   Date       : May 29, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : Write a program that computes the maximum profit that can be made by buying and selling
+#                a share at most twice. The second buy must be made on another date after the first sale.
+#
+#   Example    : input: [200, 500, 100, 200, 400, 300, 200, 150, 350, 300, 500] -> output: 700
+#
+#   Instruction
+#               1. import 5_7_p47.py
+#               2. Run the function 5_7_p47.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+
+### a function starting this script
+def start():
+    print("5_7_p47.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+start()
