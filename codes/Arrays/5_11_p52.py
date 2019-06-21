32b934f391ea2996b1219b79eb0084f227338434
Hyunjin Kim <firadazer@gmail.com>
2019-06-20 23:48:48 -0400

    5_11_p52.py completed.

--- codes/Arrays/5_11_p52.py
+++ codes/Arrays/5_11_p52.py
@@ -26,8 +26,32 @@ def start():
     print("5_11_p52.py")
 
     start_time = timeit.default_timer()
-    print("")
+    print("next_perm([1, 0, 3, 2]) = ", next_perm([1, 0, 3, 2]))
+    print("next_perm([3, 2, 1, 0]) = ", next_perm([3, 2, 1, 0]))
+    print("next_perm([3, 1, 0, 2, 4]) = ", next_perm([3, 1, 0, 2, 4]))
+    print("next_perm([2, 0, 1, 3, 4, 5]) = ", next_perm([2, 0, 1, 3, 4, 5]))
+    print("next_perm([0, 4, 2, 1, 3, 7, 6, 5]) = ", next_perm([0, 4, 2, 1, 3, 7, 6, 5]))
     print("Execution Time: ", timeit.default_timer() - start_time)
+
+
+### from the end, find the lower value than the end
+### if found, swap it with the end and swap the next right with the swapped one again
+### time complexity: O(n), space complexity: O(1)
+def next_perm(P):
+    stop_idx = -1
+    for i in reversed(range(len(P)-1)):
+        if P[i] < P[len(P)-1]:
+            stop_idx = i
+            break
+
+    if stop_idx is -1:
+        return []
+    elif len(P) - stop_idx is 2:
+        P[stop_idx], P[len(P) - 1] = P[len(P) - 1], P[stop_idx]
+        return P
+    else:
+        P[stop_idx], P[stop_idx + 1], P[len(P) - 1] = P[len(P) - 1], P[stop_idx], P[stop_idx + 1]
+        return P
 
 
 start()
