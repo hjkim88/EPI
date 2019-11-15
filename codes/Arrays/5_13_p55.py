1effc3ea8c7cd92cc46b15666aa74ec377145fa1
Hyunjin Kim <firadazer@gmail.com>
2019-11-15 11:33:13 -0500

    5_13_p55.py skeleton built.

---
+++ codes/Arrays/5_13_p55.py
@@ -0,0 +1,32 @@
+###
+#   File name  : 5_13_p55.py
+#   Author     : Hyunjin Kim
+#   Date       : Nov 15, 2019
+#   Email      : firadazer@gmail.com
+#   Purpose    : Design a program that takes as input a size k, and reads packets, continuously maintaining
+#                a uniform random subset of size k of the read packets.
+#
+#   Instruction
+#               1. import 5_13_p55.py
+#               2. Run the function 5_13_p55.start()
+#               3. The results will be generated in the console
+###
+
+### import modules
+import timeit
+import random
+
+### a function starting this script
+def start():
+    print("5_13_p55.py")
+
+    start_time = timeit.default_timer()
+    print("")
+    print("Execution Time: ", timeit.default_timer() - start_time)
+
+### a packet sniffer
+def packet_sniffer(packet, k):
+
+
+
+start()
