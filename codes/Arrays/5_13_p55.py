fbb4420e2c11d4a5932e7728e3ea6000b2cf1dc9
Hyunjin Kim <firadazer@gmail.com>
2019-11-16 08:54:43 -0500

    5_13_P55.py completed.

--- codes/Arrays/5_13_p55.py
+++ codes/Arrays/5_13_p55.py
@@ -20,13 +20,57 @@ import random
 def start():
     print("5_13_p55.py")
 
+    ### THE PACKETS ARE NOT ACTUALLY IN STREAMING HERE, SO RUNNING TIME MIGHT BE DIFFERENT TO OUR EXPECTATIONS.
+
     start_time = timeit.default_timer()
-    print("")
+    print("packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
     print("Execution Time: ", timeit.default_timer() - start_time)
 
-### a packet sniffer
-def packet_sniffer(packet, k):
+    start_time = timeit.default_timer()
+    print("packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5) = ",
+          packet_sniffer2(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'], 5))
+    print("Execution Time: ", timeit.default_timer() - start_time)
 
+
+### a packet sniffer
+### a brute-force approach
+### read packet set and select random k samples
+def packet_sniffer(packet, k):
+    idx = random.sample(range(len(packet)), k)
+    idx.sort()
+
+    return [packet[x] for x in idx]
+
+
+### a packet sniffer2
+### a more efficient approach
+### read the first k packets and just replace one of the packets while reading remaining packets in the stream
+### this can downsize the space complexity hugely and the time complexity
+def packet_sniffer2(packet, k):
+    result = packet[0:k]
+    for x in range((k+1), len(packet)):
+        idx = random.sample(range(x), 1)
+        if idx[0] < k:
+            result[idx[0]] = packet[x]
+
+    return result
 
 
 start()
